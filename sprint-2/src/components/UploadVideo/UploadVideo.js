9efc985e5685fb535121c495519151e01853bf3c
Paulo <paulovlutz@gmail.com>
2020-05-21 19:43:25 -0400

    add uploadvideo page styles for mobile breakpoint

--- sprint-2/src/components/UploadVideo/UploadVideo.js
+++ sprint-2/src/components/UploadVideo/UploadVideo.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 import Header from "../Header/Header";
+import UploadVideoMain from "../UploadVideoMain/UploadVideoMain";
 
 class UploadVideo extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <UploadVideoMain />
       </React.Fragment>
     )
   }
---
+++ sprint-2/src/components/UploadVideoMain/UploadVideoMain.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./UploadVideoMain.scss";
+import videoThumbnail from "../../assets/images/Upload-video-preview.jpg";
+
+function UploadVideoMain() {
+    return(
+        <div className="upload">
+            <hr className="upload__divider"></hr>
+            <h1 className="upload__title">Upload Video</h1>
+            <form>
+                <label htmlFor="video" className="upload__label">VIDEO THUMBNAIL</label>
+                <img className="upload__video" src={videoThumbnail} alt="Video-Thumbnail" name="video"></img>
+
+                <label htmlFor="videoTitle" className="upload__label">TITLE YOUR VIDEO</label>
+                <input className="upload__videoTitle" type="text" name="videoTitle" placeholder="Add a title to your video"></input>
+
+                <label htmlFor="comment" className="upload__label">ADD A VIDEO DESCRIPTION</label> 
+                <textarea className="upload__textarea" name="comment" cols="30" rows="5" placeholder="Add a description of your video"></textarea>
+
+                <div className="upload__buttons">
+                    <button type="submit" className="upload__submit">PUBLISH</button>
+                    <button type="submit" className="upload__cancel">CANCEL</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default UploadVideoMain;
